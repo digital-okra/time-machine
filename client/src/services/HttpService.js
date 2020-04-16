bbb9bbacaea58b24f5438b0eacff0118ce912f98
Sudharshan <sudhar393@gmail.com>
2020-04-16 00:48:53 +0000

    Update HttpService.js

--- client/src/services/HttpService.js
+++ client/src/services/HttpService.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:8000/api/v1";
-// export const baseUrl = "https://backend.timemachine.mescreators.com:443/api/v1";
+// export const baseUrl = "http://localhost:8000/api/v1";
+export const baseUrl = "https://backend.timemachine.mescreators.com:443/api/v1";
 
